9ffe6b3844c78459da2fdd7076388446f5984a73
Jurijs <Jurijs@JurijsM.ceuherning.int>
2016-12-01 10:56:26 +0100

    dbaccess > db

--- 12_01 - Banksystem/src/db/db.java
+++ 12_01 - Banksystem/src/db/db.java
@@ -5,13 +5,13 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class dbaccess {
+public class db {
     static final String db = "jdbc:hsqldb:hsql://localhost/mydb";
     static final String dbuser = "SA";
     static final String dbpass = "";
     private Connection connection = null;
 
-	public dbaccess() {
+	public db() {
 		try {
 			connection = DriverManager.getConnection(db, dbuser, dbpass);
 			connection.setAutoCommit(false);
