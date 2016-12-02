7066e1384a76f64b848b628528b7ff7f01a392b2
dennis <dennis@10.153.238.82>
2016-12-02 11:39:29 +0100

    dope

--- 12_01 - Banksystem/src/domain/NormaltLogin.java
+++ 12_01 - Banksystem/src/domain/NormaltLogin.java
@@ -1,5 +1,13 @@
 package domain;
 
-public class NormaltLogin {
+public class NormaltLogin extends Login {
+int id;
+	public NormaltLogin(String brugernavn, String adgangskode) {
+		super(brugernavn, adgangskode);
+		id=1;
+	}
+	public int getId() {
+		return id;
+	}
 
 }
