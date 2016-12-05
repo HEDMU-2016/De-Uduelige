a154b08adcaae1bdc7b09f3598fa2811f4d5703c
dennis <dennis@MSI.ceuherning.int>
2016-12-05 12:39:04 +0100

    Setter og getter til startdato og slutdato

--- 12_01 - Banksystem/src/domain/Kunde.java
+++ 12_01 - Banksystem/src/domain/Kunde.java
@@ -1,8 +1,12 @@
 package domain;
 
+import java.sql.Date;
+
 public class Kunde {
 String navn;
 String konto;
+Date startdato;
+Date slutdato;
 
 	public Kunde(String navn){
 		this.navn=navn;
@@ -19,9 +23,25 @@ String konto;
 		return navn;
 	}
 
+	public Date getStartdato() {
+		return startdato;
+	}
+
+	public void setStartdato(Date startdato) {
+		this.startdato = startdato;
+	}
+
+	public Date getSlutdato() {
+		return slutdato;
+	}
+
+	public void setSlutdato(Date slutdato) {
+		this.slutdato = slutdato;
+	}
+
 	@Override
 	public String toString() {
-		return "Kunde [navn=" + navn + ", konto=" + konto + "]";
+		return "Kunde [navn=" + navn + ", konto=" + konto + "Oprettet: "+startdato + "]";
 	}
 	
 }
