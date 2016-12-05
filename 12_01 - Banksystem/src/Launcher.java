5f7c8d14c16188aa41da96d6616b6d07325853c9
tanddant <tanddant@gmail.com>
2016-12-05 08:24:52 +0100

    PENIS

--- 12_01 - Banksystem/src/Launcher.java
+++ 12_01 - Banksystem/src/Launcher.java
@@ -4,12 +4,12 @@ import login.Login_IO;
 
 public class Launcher extends Application {
 
+	public static void main(String[] args) {
+		launch(args);
+	}
+
 	public void start(Stage loginStage) throws Exception {
 		Login_IO login_IO = new Login_IO();
 		login_IO.start(loginStage);
-	}
-
-	public static void main(String[] args) {
-		launch(args);
 	}
 }
