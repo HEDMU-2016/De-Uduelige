dd3d09315b74c28f3a4884ec9bb2f73cdf151e9c
tanddant <tanddant@gmail.com>
2016-12-08 12:22:30 +0100

    Luk knap og ikon

--- 12_01 - Banksystem/src/Brugerflade/KontoOversigt.java
+++ 12_01 - Banksystem/src/Brugerflade/KontoOversigt.java
@@ -7,9 +7,11 @@ import DB.DB;
 import domain.Konto;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -32,18 +34,34 @@ ObservableList<Konto> list;
 	
 	public void start(Stage stage)throws SQLException{
 		DB db = new DB();
-		stage = new Stage();
 		GridPane grid = new GridPane();
 		grid.setVgap(10);
 		grid.setHgap(10);
 	
+		//Dette skal bruges på alle vinduer
+		//
+		//
+		stage.setTitle("Konto Oversigt - Lortebank A/S");
+		stage.getIcons().add(new Image("Brugerflade/ico.png"));
+		//
+		//
+		//Dette skal bruges på alle vinduer
+		
+		
+		Button close = new Button("x");
+		close.setId("close");
+		grid.add(close,0,0);
+		close.setOnAction(e->{
+		stage.close();
+		});
+		
 		
 		
 		
 		
 		Scene scene = new Scene(grid);
 		stage.setScene(scene);
-		scene.getStylesheets().add(Login_IO.class.getResource("login.css").toExternalForm());
+		scene.getStylesheets().add(Brugermenu.class.getResource("Brugermenu.css").toExternalForm());
 		stage.setResizable(false);
 		stage.initStyle(StageStyle.UNDECORATED);
 		stage.show();
