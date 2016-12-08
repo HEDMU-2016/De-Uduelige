d0f4fec9abb310d34060379ebdb43d55fed6ec8d
tanddant <tanddant@gmail.com>
2016-12-08 09:24:33 +0100

    Launcheren skal jo virke xD

--- 12_01 - Banksystem/src/Launcher.java
+++ 12_01 - Banksystem/src/Launcher.java
@@ -11,10 +11,10 @@ public class Launcher extends Application {
 	}
 
 	public void start(Stage loginStage) throws Exception {
-//		Login_IO login_IO = new Login_IO();
-//		login_IO.start(loginStage);
-		Brugermenu brugermenu = new Brugermenu();
-		Stage tmpStage = new Stage();
-		brugermenu.start(tmpStage);
+		Login_IO login_IO = new Login_IO();
+		login_IO.start(loginStage);
+//		Brugermenu brugermenu = new Brugermenu();
+//		Stage tmpStage = new Stage();
+//		brugermenu.start(tmpStage);
 	}
 }
