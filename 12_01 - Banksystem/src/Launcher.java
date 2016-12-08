06e6bf1a83a223af3ccbc86b541e23b1928792bc
tanddant <tanddant@gmail.com>
2016-12-08 11:48:08 +0100

    Launcher fix

--- 12_01 - Banksystem/src/Launcher.java
+++ 12_01 - Banksystem/src/Launcher.java
@@ -12,16 +12,16 @@ public class Launcher extends Application {
 
 	public static void main(String[] args)throws SQLException {
 		
-		DB db = new DB();
-		db.listKunder();
+//		DB db = new DB();
+//		db.listKunder();
 		launch(args);
 	}
 
 	public void start(Stage loginStage) throws Exception {
-//		Login_IO login_IO = new Login_IO();
-//		login_IO.start(loginStage);
-		Brugermenu brugermenu = new Brugermenu();
-		Stage tmpStage = new Stage();
-		brugermenu.start(tmpStage);
+		Login_IO login_IO = new Login_IO();
+		login_IO.start(loginStage);
+//		Brugermenu brugermenu = new Brugermenu();
+//		Stage tmpStage = new Stage();
+//		brugermenu.start(tmpStage);
 	}
 }
