b33720f8d954e62978aca3fe8ded5a8a4aab374e
dennis <dennis@MSI.ceuherning.int>
2016-12-08 09:16:01 +0100

    små ændringer

--- 12_01 - Banksystem/src/Brugerflade/KontoOversigt.java
+++ 12_01 - Banksystem/src/Brugerflade/KontoOversigt.java
@@ -1,17 +1,37 @@
 package Brugerflade;
 
+
+import org.hsqldb.Table;
+
+import domain.Konto;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import login.Login_IO;
-
 public class KontoOversigt {
+
+	private void createTable(ObservableList<Konto> kontolist){
+	TableView<Konto> kontooversigt = new TableView<Konto>();
+	TableColumn<Konto,String> ejerCol = new TableColumn("name");
+	ejerCol.setCellValueFactory(new PropertyValueFactory<Konto,String>("Name"));
+	kontooversigt.setItems(kontolist);
+//	Table.getColumns().addAll(ejerCol);
+	}
+	
 	public void start(Stage stage){
 		stage = new Stage();
 		GridPane grid = new GridPane();
 		grid.setVgap(10);
 		grid.setHgap(10);
+	
+
+		
+		
 		
 		
 		Scene scene = new Scene(grid);
