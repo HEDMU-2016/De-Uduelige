c73904c9f56d4a0d878b1fd88b875a248b7abd77
tanddant <tanddant@gmail.com>
2016-12-09 11:11:03 +0100

    Fixede den

--- 12_01 - Banksystem/src/Brugerflade/Kundeoversigt.java
+++ 12_01 - Banksystem/src/Brugerflade/Kundeoversigt.java
@@ -4,7 +4,9 @@ import java.sql.SQLException;
 
 import DB.DB;
 import domain.Kunde;
+import domain.Login;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,8 +14,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import utill.TableCreator;
@@ -23,46 +27,43 @@ public class Kundeoversigt {
 
 
 	public void start(Stage stage) throws SQLException {
-	
-		DB db = new DB();
 		TableCreator tablecreator = new TableCreator();
-		
 		GridPane grid = new GridPane();
 		grid.setVgap(10);
 		grid.setHgap(10);
-		
-		stage.setTitle("Konto historik - Lortebank A/S");
+		grid.setAlignment(Pos.CENTER);
+
+		stage.setTitle("Kundeoversigt - Lortebank A/S");
 		stage.getIcons().add(new Image("Brugerflade/ico.png"));
 
+		Text loginlabel = new Text("Kunder");
+		HBox hbloginlabel = new HBox(10);
+		grid.add(hbloginlabel, 0, 0);
+		hbloginlabel.getChildren().add(loginlabel);
+		loginlabel.setId("overskrift");
 
-		Button close = new Button("x");
+		Button close = new Button("X");
+		HBox hbClose = new HBox(10);
 		close.setId("close");
-		grid.add(close, 10, 0);
+		hbClose.getChildren().add(close);
+		hbClose.setAlignment(Pos.TOP_RIGHT);
+		grid.add(hbClose, 1, 0);
+
 		close.setOnAction(e -> {
 			stage.close();
 		});
 
-		final Label label = new Label("Kunde oversigt");
-		label.setFont(new Font("arial", 20));
-		
 		TableView<Kunde> kundeoversigt = tablecreator.kundetable();
-		
-		kundeoversigt.autosize();
-		kundeoversigt.getOnScroll();
-		
-		final VBox vbox = new VBox();
-		vbox.setSpacing(5);
-		vbox.setPadding(new Insets(10, 0, 0, 10));
-		vbox.getChildren().addAll(label, kundeoversigt);
-		Scene scene = new Scene(new Group(), 450, 400);
-		 ((Group) scene.getRoot()).getChildren().addAll(vbox);
-	
-	 
+		kundeoversigt.setPrefWidth(350);
+//		loginoversigt.getSelectionModel().setCellSelectionEnabled(true);
+//		loginoversigt.setEditable(true);
+		grid.add(kundeoversigt, 0, 1, 2, 2);
+
+		Scene scene = new Scene(grid, 400, 500);
 		stage.setScene(scene);
 		scene.getStylesheets().add(Brugermenu.class.getResource("Brugermenu.css").toExternalForm());
 		stage.setResizable(false);
 		stage.initStyle(StageStyle.UNDECORATED);
 		stage.show();
-
 	}
 }
