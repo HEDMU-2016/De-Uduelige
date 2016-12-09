64a3792e874375a6808a84c4b33e91658ec783ca
dennis <dennis@MSI.ceuherning.int>
2016-12-09 11:21:09 +0100

    .

--- 12_01 - Banksystem/src/Launcher.java
+++ 12_01 - Banksystem/src/Launcher.java
@@ -1,56 +1,27 @@
 
 import java.sql.SQLException;
 
+import DB.DB;
+import domain.Kunde;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import login.Login_IO;
 
-public class Launcher extends Application implements Runnable  {
-	boolean running;
-	Thread thread;
+public class Launcher extends Application  {
+
 	public static void main(String[] args)throws SQLException {
+	DB db = new DB();
+	Kunde dan = new Kunde("dan", "dan@gmail.com");
+	db.addKunde(dan);
+		
 	launch(args);
 	}
 
 	public void start(Stage loginStage) throws Exception {
 		Login_IO login_IO = new Login_IO();
-		startrunning();
 		login_IO.start(loginStage);
 		
 	}
-	public synchronized void startrunning() {
-		thread = new Thread(this);
-		thread.start();
-		running = true;
-	}
-	public void stop(){
-		running=false;
-	}
-	@Override
-	public void run() {
-		long lasttime = System.nanoTime();
-		double amountOfTicks = 60.0;
-		double ns = Math.pow(10, 9) / amountOfTicks;
-		double delta = 0;
-		long timer = System.nanoTime();
-		int frames = 0;
-		while (running) {
-			long now = System.nanoTime();
-			delta += (now - lasttime) / ns;
-			lasttime = now;
-			while (delta >=1) {
-				delta--;
-				
-			}
-			if (running)
-			frames++;
-			if (System.currentTimeMillis() - timer > 1000) {
-				timer += 1000;
-				System.out.println("FPS: " + frames);
-				frames = 0;
-			}
-			stop();
-		}
-	}
+
 
 }
