f637e2c9f372160cf691b63e0c374bdbce8b81a1
dennis <dennis@192.168.1.53>
2016-12-09 18:43:21 +0100

    nothing to see here, totally didn't accidentally delete launcher somehow.

---
+++ 12_01 - Banksystem/src/Brugerflade/Launcher.java
@@ -0,0 +1,24 @@
+package Brugerflade;
+
+import java.sql.SQLException;
+
+import DB.DB;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import login.Login_IO;
+
+public class Launcher extends Application  {
+
+	public static void main(String[] args)throws SQLException {	
+		System.out.println(System.currentTimeMillis());
+		launch(args);
+	}
+
+	public void start(Stage loginStage) throws Exception {
+		Login_IO login_IO = new Login_IO();
+		login_IO.start(loginStage);
+		
+	}
+
+
+}
