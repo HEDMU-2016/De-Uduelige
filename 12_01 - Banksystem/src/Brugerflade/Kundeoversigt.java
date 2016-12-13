da43d9dbdce3521a03428128e49d61457679608c
tanddant <tanddant@gmail.com>
2016-12-13 09:20:21 +0100

    Kundeoversigt layout

--- 12_01 - Banksystem/src/Brugerflade/Kundeoversigt.java
+++ 12_01 - Banksystem/src/Brugerflade/Kundeoversigt.java
@@ -26,8 +26,6 @@ import utill.TableCreator;
 
 public class Kundeoversigt {
 
-
-
 	public void start(Stage stage) throws SQLException {
 		TableCreator tablecreator = new TableCreator();
 		DB db = new DB();
@@ -58,51 +56,53 @@ public class Kundeoversigt {
 
 		TableView<Kunde> kundeoversigt = tablecreator.kundetable();
 		kundeoversigt.setPrefWidth(350);
-//		loginoversigt.getSelectionModel().setCellSelectionEnabled(true);
-//		loginoversigt.setEditable(true);
+		// loginoversigt.getSelectionModel().setCellSelectionEnabled(true);
+		// loginoversigt.setEditable(true);
 		grid.add(kundeoversigt, 0, 1, 1, 4);
-		
+
 		Label navnlabel = new Label("Navn");
 		navnlabel.setId("tekst");
 		grid.add(navnlabel, 2, 1);
-		
+
 		TextField navnfelt = new TextField();
+		navnfelt.setPrefWidth(200);
 		grid.add(navnfelt, 3, 1);
-		
+
 		Label emaillabel = new Label("Email");
 		emaillabel.setId("tekst");
 		grid.add(emaillabel, 2, 2);
-		
+
 		TextField emailfelt = new TextField();
 		grid.add(emailfelt, 3, 2);
-		
+
 		Label brugernavnlabel = new Label("Brugernavn");
 		brugernavnlabel.setId("tekst");
 		grid.add(brugernavnlabel, 2, 3);
-		
+
 		TextField brugernavnfelt = new TextField();
 		grid.add(brugernavnfelt, 3, 3);
-		
+
 		Label fejl = new Label("");
 		fejl.setId("fejl");
 		grid.add(fejl, 2, 5, 3, 5);
-		
+
 		Button opret = new Button("Opret");
 		grid.add(opret, 2, 4, 3, 4);
 		opret.setId("KnapImenu");
-		opret.setOnAction(e->{
-		if(navnfelt.getText().isEmpty() == false && brugernavnfelt.getText().isEmpty() == false && emailfelt.getText().isEmpty() == false)
-		try {
-			db.addKunde(new Kunde(navnfelt.getText(), emailfelt.getText(), brugernavnfelt.getText()));
-			fejl.setTextFill(Color.GREEN);
-			fejl.setText("Kunden er nu oprettet!");
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}	
-		else{
-			fejl.setTextFill(Color.RED);
-			fejl.setText("Du skal lige udfylde alle felterne!");
-		}
+		opret.setOnAction(e -> {
+			if (navnfelt.getText().isEmpty() == false && brugernavnfelt.getText().isEmpty() == false
+					&& emailfelt.getText().isEmpty() == false)
+				try {
+					db.addKunde(new Kunde(navnfelt.getText(), emailfelt.getText(), brugernavnfelt.getText()));
+					fejl.setTextFill(Color.GREEN);
+					fejl.setText("Kunden er nu oprettet!");
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+			else {
+				fejl.setTextFill(Color.RED);
+				fejl.setText("Du skal lige udfylde alle felterne!");
+			}
 		});
 
 		Scene scene = new Scene(grid);
