358a009a6c9f6850f699ecf4e698bd021e8b0c9d
tanddant <tanddant@gmail.com>
2016-12-15 12:52:16 +0100

    Så er den sku smuk

--- 12_01 - Banksystem/src/Brugerflade/Kontaktbog.java
+++ 12_01 - Banksystem/src/Brugerflade/Kontaktbog.java
@@ -2,6 +2,8 @@ package Brugerflade;
 
 import java.sql.SQLException;
 
+import com.sun.prism.paint.Color;
+
 import DB.DB;
 import domain.Kontakt;
 import domain.Login;
@@ -22,16 +24,16 @@ import javafx.stage.WindowEvent;
 import utill.TableCreator;
 
 public class Kontaktbog {
-	public void start(Stage stage,Login bruger) throws SQLException{
+	public void start(Stage stage, Login bruger) throws SQLException {
 		DB db = new DB();
 		TableCreator tablecreator = new TableCreator();
-		
+
 		GridPane grid = new GridPane();
 		grid.setVgap(10);
 		grid.setHgap(10);
 		grid.setAlignment(Pos.CENTER);
 
-		stage.setTitle("NAVN PÅ VINDEUET - Lortebank A/S");
+		stage.setTitle("Kontaktbog - Lortebank A/S");
 		stage.getIcons().add(new Image("Brugerflade/ico.png"));
 
 		Button close = new Button("X");
@@ -39,60 +41,65 @@ public class Kontaktbog {
 		close.setId("close");
 		hbClose.getChildren().add(close);
 		hbClose.setAlignment(Pos.TOP_RIGHT);
-		grid.add(hbClose, 0, 0);
+		grid.add(hbClose, 3, 0);
 
 		close.setOnAction(e -> {
 			stage.close();
 		});
 		TableView<Kontakt> kontakttable = tablecreator.kontaktTable(bruger);
-		grid.add(kontakttable, 1, 1);
-		
-		Label navnlabel = new Label("navn");
-		grid.add(navnlabel, 1, 8);
-		
+		grid.add(kontakttable, 0, 0, 1, 5);
+
+		Label navnlabel = new Label("Navn");
+		grid.add(navnlabel, 2, 1);
+
 		TextField navnfelt = new TextField();
-		grid.add(navnfelt, 1, 9);
-		
-		Label kontonrlabel = new Label("kontonr");
-		grid.add(kontonrlabel, 2, 8);
-		
+		navnfelt.setPrefWidth(250);
+		grid.add(navnfelt, 3, 1);
+
+		Label kontonrlabel = new Label("Kontonr");
+		grid.add(kontonrlabel, 2, 2);
+
 		TextField kontonrfelt = new TextField();
-		grid.add(kontonrfelt, 2, 9);
-		
-		Text notifikation = new Text();
-		grid.add(notifikation, 4, 9);
-		
-		
-		
-		Button gemknap = new Button("tilfør kontakt");
-		grid.add(gemknap, 3, 8);
-		gemknap.setOnAction(e->{
-		Kontakt nykontakt = new Kontakt(navnfelt.getText(),Integer.parseInt(kontonrfelt.getText()));
-		try {
-			db.addKontakt(nykontakt, bruger.getBrugernavn());
-			if(kontonrfelt.getText().isEmpty())
-			notifikation.setText("kontonr skal være udfyldt");	
-			else
-			notifikation.setText("luk og åbn din kontaktbog for at se dine opdateringer");
-			
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		
+		grid.add(kontonrfelt, 3, 2);
+
+		Text notifikation = new Text("TEST TEST TEST TEST TEST");
+		notifikation.setId("fejl");
+		grid.add(notifikation, 2, 6, 3, 6);
+
+		Button gemknap = new Button("Tilføj kontakt");
+		gemknap.setId("KnapImenu");
+		grid.add(gemknap, 2, 4, 3, 4);
+		gemknap.setOnAction(e -> {
+			Kontakt nykontakt = new Kontakt(navnfelt.getText(), Integer.parseInt(kontonrfelt.getText()));
+			try {
+				db.addKontakt(nykontakt, bruger.getBrugernavn());
+				if (kontonrfelt.getText().isEmpty()){
+					//notifikation.setFill(Color.RED);
+					notifikation.setText("kontonr skal være udfyldt");
+				}else{
+					//notifikation.setFill(Color.GREEN);
+					notifikation.setText("luk og åbn din kontaktbog for at se dine opdateringer");
+				}
+					
+
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+
 		});
-		
-		Scene scene = new Scene(grid, 400, 400);
+
+		Scene scene = new Scene(grid);
 		stage.setScene(scene);
 		scene.getStylesheets().add(Brugermenu.class.getResource("Brugermenu.css").toExternalForm());
 		stage.setResizable(false);
 		stage.initStyle(StageStyle.UNDECORATED);
 		stage.show();
-		
+
 		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                event.consume();
-            }
-        });
+			@Override
+			public void handle(WindowEvent event) {
+				event.consume();
+			}
+		});
 	}
 }
